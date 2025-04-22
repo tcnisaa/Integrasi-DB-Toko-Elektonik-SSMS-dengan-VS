089a2b28b6d60dd3bc5bb1b057292618a341cc4e
nisa <tangguhchairunnisa04@gmail.com>
2025-04-22 14:50:22 +0700

    first commit

---
+++ TokoElektronik/ListItems.aspx
@@ -0,0 +1,43 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="ListItems.aspx.cs" Inherits="TokoElektronik.ListItems" %>
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Toko Elektronik Orders</title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <h1 style="text-align:center; color:blue;">Kelompok 60_Shift 6</h1>
+        <h2>Tambah Order</h2>
+        <table>
+            <tr>
+                <td>Order ID :</td>
+                <td><asp:TextBox ID="txtOrderID" runat="server"></asp:TextBox></td>
+            </tr>
+            <tr>
+                <td>Customer Name :</td>
+                <td><asp:TextBox ID="txtCustomerName" runat="server"></asp:TextBox></td>
+            </tr>
+            <tr>
+                <td>Quantity :</td>
+                <td><asp:TextBox ID="txtQuantity" runat="server"></asp:TextBox></td>
+            </tr>
+            <tr>
+                <td>Product ID :</td>
+                <td><asp:TextBox ID="txtProductID" runat="server"></asp:TextBox></td>
+            </tr>
+            <tr>
+                <td></td>
+                <td>
+                    <asp:Button ID="btnAdd" runat="server" Text="ADD" OnClick="btnAdd_Click" />
+                    <asp:Button ID="btnDelete" runat="server" Text="DELETE" OnClick="btnDelete_Click" />
+                    <asp:Button ID="btnUpdate" runat="server" Text="UPDATE" OnClick="btnUpdate_Click" />
+                    <asp:Button ID="btnClear" runat="server" Text="CLEAR" OnClick="btnClear_Click" />
+                </td>
+            </tr>
+        </table>
+
+        <h3>Data Orders</h3>
+        <asp:GridView ID="GridViewOrders" runat="server"></asp:GridView>
+    </form>
+</body>
+</html>
---
+++ TokoElektronik/ListItems.aspx.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace TokoElektronik
+{
+    public partial class ListItems : System.Web.UI.Page
+    {
+        SqlConnection con = new SqlConnection();
+        SqlCommand cmd = new SqlCommand();
+        SqlDataAdapter sda = new SqlDataAdapter();
+        DataSet ds = new DataSet();
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            con.ConnectionString = "Data Source=DESKTOP-ACA\\SQLEXPRESS; Initial Catalog=TokoElektronik; Integrated Security=True";
+            con.Open();
+            if (!Page.IsPostBack)
+            {
+                DataShow();
+            }
+        }
+
+        public void DataShow()
+        {
+            ds = new DataSet();
+            cmd.CommandText = "SELECT * FROM vw_OrderDetails";
+            cmd.Connection = con;
+            sda = new SqlDataAdapter(cmd);
+            sda.Fill(ds);
+            GridViewOrders.DataSource = ds;
+            GridViewOrders.DataBind();
+            con.Close();
+        }
+
+        protected void btnAdd_Click(object sender, EventArgs e)
+        {
+            cmd.CommandText = "INSERT INTO Orders (CustomerName, Quantity, ProductID) VALUES ('" + txtCustomerName.Text + "', '" + txtQuantity.Text + "', '" + txtProductID.Text + "')";
+            cmd.Connection = con;
+            cmd.ExecuteNonQuery();
+            DataShow();
+        }
+
+        protected void btnUpdate_Click(object sender, EventArgs e)
+        {
+            cmd.CommandText = "UPDATE Orders SET CustomerName='" + txtCustomerName.Text + "', Quantity='" + txtQuantity.Text + "', ProductID='" + txtProductID.Text + "' WHERE OrderID='" + txtOrderID.Text + "'";
+            cmd.Connection = con;
+            cmd.ExecuteNonQuery();
+            DataShow();
+        }
+
+        protected void btnDelete_Click(object sender, EventArgs e)
+        {
+            cmd.CommandText = "DELETE FROM Orders WHERE OrderID='" + txtOrderID.Text + "'";
+            cmd.Connection = con;
+            cmd.ExecuteNonQuery();
+            DataShow();
+        }
+
+        protected void btnClear_Click(object sender, EventArgs e)
+        {
+            txtOrderID.Text = null;
+            txtCustomerName.Text = null;
+            txtQuantity.Text = null;
+            txtProductID.Text = null;
+        }
+    }
+}
